27d8f40f3b41686c2752f882c462ff135a454d2b
Monnut <mast.monnut.boon@gmai.com>
2024-11-09 17:36:36 +0700

    End of section 11

--- API/Extensions/ApplicationServiceExtentions.cs
+++ API/Extensions/ApplicationServiceExtentions.cs
@@ -1,5 +1,6 @@
 using System;
 using API.Data;
+using API.Helpers;
 using API.Interfaces;
 using API.Services;
 using Microsoft.EntityFrameworkCore;
@@ -22,7 +23,9 @@ public static class ApplicationServiceExtentions
         services.AddCors();
         services.AddScoped<ITokenService, TokenService>();   //add once per client request
         services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IPhotoService, PhotoService>();
         services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        services.Configure<CloudinarySettings>(config.GetSection("CloudinarySetting"));
         return services;
 
     }
